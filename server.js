ba802e14ef63c5c5dfa9d23b35325be1428eaa24
Tina Nguyen <nguyentinaca@yahoo.com>
2021-03-10 10:53:14 -0800

    find by Id

--- server.js
+++ server.js
@@ -50,6 +50,21 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result
+}
